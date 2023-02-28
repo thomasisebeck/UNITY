930938dc7bb5093447637c301d33dce369b97901
thomasisebeck <thomasisebeck@gmail.com>
2023-02-28 09:07:58 +0200

    created pipes

--- The Unity Tutorial For Complete Beginners/Assets/PipeSpawnScript.cs
+++ The Unity Tutorial For Complete Beginners/Assets/PipeSpawnScript.cs
@@ -1,27 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PipeSpawnScript : MonoBehaviour
 {
-
+    //reference to prefab, filled slot in prefab
     public GameObject myPipePrefab;
-    public int spawnInterval; 
+    public int spawnDelay;
+    private float timer = 0;
+    private float randomDelay = 1; 
+
+    void CreatePipe()
+    {
+        //create a random offset
+        float randomYOffset = Random.Range(-10.0f, 8.6f);
+        Vector3 spawnPosition = new Vector3(transform.position.x, randomYOffset);
+
+        //spawn the prefab with the original position and rotation
+        Instantiate(myPipePrefab, spawnPosition, transform.rotation);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("CreatePipe", 5, spawnInterval);
-    }
-
-    void CreatePipe()
-    {
-        Instantiate(myPipePrefab, new Vector3(33,0,0), Quaternion.identity); //identity rotation
+        CreatePipe(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < spawnDelay + randomDelay)
+        {
+            timer += Time.deltaTime; //add time to the timer if less than spawnrate
+            return;
+        }
+
+        //reset to 0 and spawn a pipe
+        timer = 0;
+        randomDelay = Random.Range(0, 4);
+        CreatePipe();
     }
 }
